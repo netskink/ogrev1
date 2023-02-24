f8b3b7bba24f0aaff204bb6ef4586658525ffccf
John F. Davis <john.davis@broadcom.com>
2023-02-24 11:59:11 -0500

    added file IO to read and decode json

--- ogrev1/ogrev1/fileIO.swift
+++ ogrev1/ogrev1/fileIO.swift
@@ -7,31 +7,87 @@
 
 import Foundation
 
+
+
 struct UnitData: Decodable {
-    var name: String
+    var name: String = "none"
+    var race: String = "none"
+    // values are 1-100. Initially.  Can scale during combat to be greater than 100
+    var offense: Double = 0.0
+    var defense: Double = 0.0
+    var damage: Double = 0.0
+    var stamina: Double = 0.0
+    var morale: Double = 0.0
+    // values are 0-50
+    var hp: Double = 0.0
 }
 
-/*
- 
- https://stackoverflow.com/a/36827996/1008596
- 
- 
-func my_read_file(file_name: String) -> [UnitData]? {
+struct UnitsData: Decodable {
+    var units: [UnitData]
+}
+
+
+
+
+// my first attemp
+//
+// https://stackoverflow.com/a/36827996/1008596
+
+
+enum JFDFileError: Error {
+    case FileNotFound
+    case JSONDecodeFailed
+    case UnknownError
+}
+
+
+// https://www.swiftbysundell.com/articles/working-with-files-and-folders-in-swift/
+// https://github.com/JohnSundell/Files/blob/master/Sources/Files.swift
+//func my_simple_file_read(path: String) throws -> UnitsData {
+func my_simple_file_read(path: String) throws -> UnitsData {
+
+    print("path is \(path)")
     
+    let file_name = (path as NSString).expandingTildeInPath
     print("file_name is \(file_name)")
 
-    if let url = Bundle.main.url(forResource: file_name, withExtension: "json") {
-        
-        do {
-            let data = try Data(contentsOf: url)
-            let decoder  = JSONDecoder()
-            let jsonData = try decoder.decode(UnitData.self, from: data)
-            print(jsonData.name)
-            return jsonData
-        } catch {
-            print("error: \(error)")
-        }
+    
+    let url = URL(fileURLWithPath: file_name)
+    print("url is \(url)")
+    
+    // Read the file
+    var data: Data
+    do {
+        data = try Data(contentsOf: url)
+        print("data is \(data)")
+    } catch {
+        throw JFDFileError.FileNotFound
     }
-    return nil
+
+    var unitsData: UnitsData
+    do {
+        let decoder = JSONDecoder()
+        unitsData = try decoder.decode(UnitsData.self, from: data)
+        print("json data is \(unitsData)")
+        print("json data.name is \(unitsData.units[1].name)")
+    } catch {
+        throw JFDFileError.JSONDecodeFailed
+    }
+
+
+
+
+    print("===.name is \(unitsData.units[1].name)")
+    return unitsData
+    
 }
-*/
+
+
+func mySimpleFileRead(path: String) throws -> UnitsData {
+        
+    let fileName = (path as NSString).expandingTildeInPath
+    let url = URL(fileURLWithPath: fileName)
+   
+    let data = try Data(contentsOf: url)
+    return try JSONDecoder().decode(UnitsData.self, from: data)
+}
--- ogrev1/ogrev1/main.swift
+++ ogrev1/ogrev1/main.swift
@@ -11,41 +11,34 @@ import Foundation
 
 
 
-func stage_fight() -> (Int, Int) {
+func stage_fight(id1:Int, id2:Int) throws -> (Int, Int) {
     print("-----")
 
-    let unit1 = Unit(name: "Cartman",
-                     offense: 50,
-                     defense: 50,
-                     damage: 10,
-                     stamina: 50,
-                     morale: 50,
-                     hp: 50)
-    let unit2 = Unit(name: "Kyle",
-                     offense: 50,
-                     defense: 50,
-                     damage: 10,
-                     stamina: 50,
-                     morale: 50,
-                     hp: 50)
+    // Read units from json file
+    var unitsData: UnitsData
+    do {
+        unitsData = try  my_simple_file_read(path: "~/progs/myxcode/ogrev1/ogrev1/ogrev1/units.json")
+    } catch {
+        throw JFDFileError.UnknownError
+    }
+    
+    
+    let unit1 = Unit(name: unitsData.units[id1].name,
+                     offense: unitsData.units[id1].offense,
+                     defense: unitsData.units[id1].defense,
+                     damage: unitsData.units[id1].damage,
+                     stamina: unitsData.units[id1].stamina,
+                     morale: unitsData.units[id1].morale,
+                     hp: unitsData.units[id2].hp)
+    let unit2 = Unit(name: unitsData.units[id2].name,
+                     offense: unitsData.units[id2].offense,
+                     defense: unitsData.units[id2].defense,
+                     damage: unitsData.units[id2].damage,
+                     stamina: unitsData.units[id2].stamina,
+                     morale: unitsData.units[id2].morale,
+                     hp: unitsData.units[id2].hp)
 
     
-/*
-    let unit1 = Unit(name: "dwarf",
-                     offense: 50,
-                     defense: 80,
-                     damage: 10,
-                     stamina: 90,
-                     morale: 80,
-                     hp: 40)
-    let unit2 = Unit(name: "orc",
-                     offense: 70,
-                     defense: 50,
-                     damage: 15,
-                     stamina:70,
-                     morale: 60,
-                     hp: 50)
-*/
  
     let combat = Combat(unit1: unit1, unit2: unit2)
     
@@ -74,6 +67,12 @@ func stage_fight() -> (Int, Int) {
     return (rounds, winner)
 }
 
+
+// ///////////////////
+//  main
+// ///////////////////
+
+
 var rounds = 0
 var winner = 0
 let NUM_TRIALS = 100
@@ -85,7 +84,12 @@ var num_times_orc_wins = 0
 
 while trials > 0 {
 
-    (rounds, winner) = stage_fight()
+    do {
+        (rounds, winner) = try stage_fight(id1:0, id2:1)
+    } catch {
+        throw JFDFileError.JSONDecodeFailed
+
+    }
 
     avg_num_rounds += rounds
     
@@ -107,4 +111,4 @@ print("unit2 won \(num_times_orc_wins)")
 
 
 
-//my_read_file(file_name: "unit")
+
